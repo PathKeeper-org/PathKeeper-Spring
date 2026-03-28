10db4fc04ea717cc46a9698a71a5d62e8e6cfccb
taeik <kti1452@naver.com>
2026-03-28 18:43:41 +0900

    chore: Spring Boot 초기 프로젝트 뼈대 생성

---
+++ src/main/java/com/pathkeeper/backend/PathkeeperBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pathkeeper.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathkeeperBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathkeeperBackendApplication.class, args);
+	}
+
+}
